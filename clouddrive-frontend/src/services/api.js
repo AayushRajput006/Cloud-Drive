47f2914a3768fd0c726ea5f771da690a376191ce
Aayush Raj <aayushsbrajput@gmail.com>
2026-06-04 02:25:10 +0530

    Final chnages 2

--- clouddrive-frontend/src/services/api.js
+++ clouddrive-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://cloud-drive-1.onrender.com",
+  baseURL:"https://cloud-drive-1.onrender.com",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json"
